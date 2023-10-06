44016317fe1f33be16e859948ccb971d34b03c27
SmzFz <felipesalmazo123@gmail.com>
2023-10-06 16:27:36 -0300

    desenvolvendo projeto

--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -1,12 +1,12 @@
 import styles from "./Nav.module.css";
-
+import logo from "../../assets/logo.svg"
 
 const Nav = () => {
   return (
     <div className={styles.bar}>
         <div className={styles.logo}>
             <img src={logo} alt="logo Petshop" width={""} />
-            LittlePet
+
 
         </div>
         <input type="text" placeholder="Buscar" />
